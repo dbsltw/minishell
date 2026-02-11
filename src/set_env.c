907d7d8a07000245e647bfc88d34d4d877de86e4
akyung123 <kik081212@gmail.com>
2026-02-11 15:49:07 +0900

    refactor: split set_env and update file names

--- src/set_env.c
+++ src/set_env.c
@@ -6,11 +6,12 @@
 /*   By: akkim <akkim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 17:26:48 by akkim             #+#    #+#             */
-/*   Updated: 2026/02/11 15:44:37 by akkim            ###   ########.fr       */
+/*   Updated: 2026/02/11 15:47:14 by akkim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "env.h"
 // env setting funtion for minishell
 // linked list
 
@@ -60,11 +61,9 @@ void	init_env(t_info_env *env, char **envp)
 	env->head = NULL;
 	while (envp[i])
 	{
-		// '=' 위치 찾기
 		eq = ft_strchr(envp[i], '=');
 		if (eq)
 		{
-			// 앞은 key, 뒤는 value로 자르기
 			key = ft_substr(envp[i], 0, eq - envp[i]);
 			value = ft_strdup(eq + 1);
 			env_add_back(&env->head, new_env_node(key, value));
@@ -72,59 +71,6 @@ void	init_env(t_info_env *env, char **envp)
 			free(value);
 		}
 		i++;
-	}
-}
-
-// find node by key
-t_env	*find_env_node(t_env *head, char *key)
-{
-	while (head)
-	{
-		if (ft_strlen(head->key) == ft_strlen(key) && \
-			ft_strncmp(head->key, key, ft_strlen(key)) == 0)
-			return (head);
-		head = head->next;
-	}
-	return (NULL);
-}
-
-// get value
-char	*get_env_val(t_env *head, char *key)
-{
-	t_env	*node;
-
-	node = find_env_node(head, key);
-	if (node)
-		return (node->value);
-	return (NULL);
-}
-
-// add env
-
-// remove env
-void	remove_env(t_info_env *env, char *key)
-{
-	t_env	*curr;
-	t_env	*prev;
-
-	curr = env->head;
-	prev = NULL;
-	while (curr)
-	{
-		if (ft_strncmp(curr->key, key, ft_strlen(key) + 1) == 0)
-		{
-			if (prev)
-				prev->next = curr->next;
-			else
-				env->head = curr->next;
-			free(curr->key);
-			if (curr->value)
-				free(curr->value);
-			free(curr);
-			return ;
-		}
-		prev = curr;
-		curr = curr->next;
 	}
 }
 
