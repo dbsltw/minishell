960fbd05e8fd95972e4424d149ed84a7913d10c2
akyung123 <kik081212@gmail.com>
2026-02-11 17:16:54 +0900

    refactor : remove duplication funtion in ft_printf

--- libft/ft_put_fuction.c
+++ libft/ft_put_fuction.c
@@ -6,40 +6,12 @@
 /*   By: akkim <akkim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 18:12:38 by akkim             #+#    #+#             */
-/*   Updated: 2026/01/19 18:12:57 by akkim            ###   ########.fr       */
+/*   Updated: 2026/02/11 17:12:34 by akkim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 #include "libft.h"
-
-void	ft_putnbr_fd(int n, int fd)
-{
-	char	num;
-
-	if (n == -2147483648)
-	{
-		write(fd, "2147483648", 10);
-		return ;
-	}
-	else if (n == 0)
-	{
-		write(fd, "0", 1);
-		return ;
-	}
-	else if (n < 0)
-		n *= -1;
-	if (n > 9)
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
-	}
-	else
-	{
-		num = '0' + n;
-		write(fd, &num, 1);
-	}
-}
 
 void	ft_putunbr_fd(unsigned int n, int fd)
 {
@@ -55,15 +27,4 @@ void	ft_putunbr_fd(unsigned int n, int fd)
 		num = '0' + n;
 		write(fd, &num, 1);
 	}
-}
-
-void	ft_putstr_fd(char *s, int fd)
-{
-	size_t	i;
-	size_t	len;
-
-	len = ft_strlen(s);
-	i = 0;
-	while (i < len)
-		ft_putchar_fd(s[i++], fd);
 }
